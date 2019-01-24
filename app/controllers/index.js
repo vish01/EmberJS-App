0ff55dcfdd102eb5e5ad076fbf0342503aee3ffb
Vishal Limbachiya <vishallimbachiya379@gmail.com>
2019-01-24 14:53:00 -0500

    made computed property

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,11 @@
 import Controller from '@ember/controller';
+import { match, not } from '@ember/object/computed';
 
 export default Controller.extend({
 
-    isDisabled: true
+  emailAddress: '',
+
+  isValid: match('emailAddress', /^.+@.+\..+$/),
+  isDisabled: not('isValid')
+
 });
