c8cd294e025a14d1d1a6f2d295de6eb5c95dec84
Vishal Limbachiya <vishallimbachiya379@gmail.com>
2019-01-24 18:05:09 -0500

    changing states

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,9 +1,17 @@
 import Controller from '@ember/controller';
 import { match, not } from '@ember/object/computed';
+import { computed, observer } from '@ember/object';
 
 export default Controller.extend({
-
+    // isDisabled: true,
   emailAddress: '',
+
+  actualEmailAddress: computed('emailAddress', function(){
+    console.log('actualEmailAddress function is called ', this.get('emailAddress'));
+  }),
+  emailAddressChanged: observer('emailAddress', function(){
+      console.log('Observer is called', this.get('emailAddress'));
+  }),
 
   isValid: match('emailAddress', /^.+@.+\..+$/),
   isDisabled: not('isValid')
