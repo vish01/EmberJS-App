46f20d45f5033fd687e885f2343dda747da3cb34
Vishal Limbachiya <vishallimbachiya379@gmail.com>
2019-01-25 14:01:15 -0500

    Added libraries

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,9 +1,12 @@
 import Controller from '@ember/controller';
-import { match, not } from '@ember/object/computed';
 import { computed, observer } from '@ember/object';
+import { empty } from '@ember/object/computed';
+import { match, not } from '@ember/object/computed';
+
 
 export default Controller.extend({
     // isDisabled: true,
+    responseMessage: '',
   emailAddress: '',
 
   actualEmailAddress: computed('emailAddress', function(){
@@ -13,7 +16,20 @@ export default Controller.extend({
       console.log('Observer is called', this.get('emailAddress'));
   }),
 
-  isValid: match('emailAddress', /^.+@.+\..+$/),
-  isDisabled: not('isValid')
+//   isValid: match('emailAddress', /^.+@.+\..+$/),
+//   isDisabled: not('isValid')
+// isDisabled: empty('emailAddress')
+    isValid: match('emailAddress', /^.+@.+\..+$/),
+     isDisabled: not('isValid'),
+
+    actions:{
+        saveInvitation(){
+            alert(`Saving of the following email address is in Progress: ${this.get('emailAddress')}`);
+            this.set('responseMessage', `Thank You! We have just saved your email address: ${this.get('emailAddress')}`);
+            this.set('emailAddress');
+        }
+    }
+
+
 
 });
