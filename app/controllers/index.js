3a6689ecfbec253edfe9429330b1f1da2057459d
Vishal Limbachiya <vishallimbachiya379@gmail.com>
2019-01-27 11:10:38 -0500

    Google Firebase
    Setup google firebase and installed email functionality. Now the database is receiving all registered emails.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,32 +1,40 @@
 import Controller from '@ember/controller';
-import { computed, observer } from '@ember/object';
-import { empty } from '@ember/object/computed';
+// import { computed, observer } from '@ember/object';
+// import { empty } from '@ember/object/computed';
 import { match, not } from '@ember/object/computed';
+// import ENV from 'config/environemt'
 
 
 export default Controller.extend({
     // isDisabled: true,
+    headerMessage: 'Coming Soon',
     responseMessage: '',
   emailAddress: '',
+  isValid: match('emailAddress', /^.+@.+\..+$/),
+  isDisabled: not('isValid'),
 
-  actualEmailAddress: computed('emailAddress', function(){
-    console.log('actualEmailAddress function is called ', this.get('emailAddress'));
-  }),
-  emailAddressChanged: observer('emailAddress', function(){
-      console.log('Observer is called', this.get('emailAddress'));
-  }),
+
+//   actualEmailAddress: computed('emailAddress', function(){
+//     console.log('actualEmailAddress function is called ', this.get('emailAddress'));
+//   }),
+//   emailAddressChanged: observer('emailAddress', function(){
+//       console.log('Observer is called', this.get('emailAddress'));
+//   }),
 
 //   isValid: match('emailAddress', /^.+@.+\..+$/),
 //   isDisabled: not('isValid')
 // isDisabled: empty('emailAddress')
-    isValid: match('emailAddress', /^.+@.+\..+$/),
-     isDisabled: not('isValid'),
+
 
     actions:{
         saveInvitation(){
-            alert(`Saving of the following email address is in Progress: ${this.get('emailAddress')}`);
+            const email = this.get('emailAddress');
+            const newInvitation = this.store.createRecord('invitation', {email: email});
+            newInvitation.save();
+
+            // alert(`Saving of the following email address is in Progress: ${this.get('emailAddress')}`);
             this.set('responseMessage', `Thank You! We have just saved your email address: ${this.get('emailAddress')}`);
-            this.set('emailAddress');
+            this.set('emailAddress', '');
         }
     }
 
--- config/environment.js
+++ config/environment.js
@@ -2,10 +2,26 @@
 
 module.exports = function(environment) {
   let ENV = {
-    modulePrefix: 'ember-js-app',
-    environment,
-    rootURL: '/',
-    locationType: 'auto',
+      modulePrefix: 'ember-js-app',
+      environment: environment,
+      rootURL: "/",
+      locationType: "auto",
+  
+      firebase: {
+        apiKey: "AIzaSyDgnnDEDAFdi7dKtu9y4IhlR2FMd9hyFYo",
+        authDomain: "myapp-1523725346458.firebaseapp.com",
+        databaseURL: "https://myapp-1523725346458.firebaseio.com",
+        projectId: "myapp-1523725346458",
+        storageBucket: "myapp-1523725346458.appspot.comm",
+        messagingSenderId: "428963497844"
+      },
+  
+      // if using ember-cli-content-security-policy
+      contentSecurityPolicy: {
+        'script-src': "'self' 'unsafe-eval' apis.google.com",
+        'frame-src': "'self' https://*.firebaseapp.com",
+        'connect-src': "'self' wss://*.firebaseio.com https://*.googleapis.com"
+      },
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
