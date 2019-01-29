59d8b1eaf173d06a0cee2e515c82869492b8b984
Vishal Limbachiya <vishallimbachiya379@gmail.com>
2019-01-29 18:41:02 -0500

    added delete and edit functionalities

--- app/router.js
+++ app/router.js
@@ -13,10 +13,12 @@ Router.map(function() {
 
   this.route('admin', function() {
     this.route('invitations');
+    this.route('contacts');
   });
 
   this.route('libraries', function() {
     this.route('new');
+    this.route('edit', { path: '/:library_id/edit' });
   });
 });
 
